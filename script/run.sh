46605f64e80134ad70f7acc7c78fb6426141b782
Seun Oh <seunoh@getmiso.com>
2018-01-25 19:30:48 +0900

    Fix script

--- script/run.sh
+++ script/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 already_remove() {
-    if [ "$(docker ps -f NAME=$NAME | wc -l)" -gt 0 ]; then
-        docker rm $NAME
+    if [ "$(docker ps -f NAME=${NAME} | wc -l)" -gt 0 ]; then
+        docker rm ${NAME}
         echo "Delete docker container ${NAME}"
     else
         echo 'Not found containers'
@@ -11,16 +11,16 @@ already_remove() {
 
 run() {
     already_remove
-    id=$(docker create --name ${NAME} -e KEY_FILE=/app/${FILENAME} -e STORE_PASSWORD=miso12 -e KEY_ALIAS=miso -e KEY_PASSWORD=miso12 ${IMAGE} release) \
-    && docker cp ${FILE} $id:/app/${FILENAME} \
-    && docker start -a -i $id \
-    && docker cp $id:/app/app/build/outputs/ $(pwd)/build/outputs/ \
-    && docker rm -f -v $id
+    ID=$(docker create --name ${NAME} -e KEY_FILE=/app/${FILENAME} -e STORE_PASSWORD=miso12 -e KEY_ALIAS=miso -e KEY_PASSWORD=miso12 ${IMAGE} release)
+    docker cp ${FILE} ${ID}:/app/${FILENAME}
+    docker start -a -i ${ID}
+    docker cp ${ID}:/app/app/build/outputs/ $(pwd)/build/outputs/
+    docker rm -f -v ${ID}
 }
 
 while getopts i:n:f: arg
 do
-    case $arg in
+    case ${arg} in
         i)
             IMAGE=$OPTARG
             echo "option i, argument <$IMAGE>"
